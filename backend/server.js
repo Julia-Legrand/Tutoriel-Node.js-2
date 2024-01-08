f35cc7af2c7f72de7f15e7c17f092c9f2275b93c
Julia-Legrand <julia-legrand@orange.fr>
2024-01-08 09:05:14 +0100

    Ajout de commentaires

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
 const port = 5000;
 
 // Connexion à la DB
@@ -15,4 +15,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/post", require("./routes/post.routes"));
 
 // Lancer le serveur
-app.listen(port, () => console.log("Le serveur a démarré au port " + port))
+app.listen(port, () => console.log("Le serveur a démarré au port " + port));
